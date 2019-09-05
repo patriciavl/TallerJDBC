fc77e8024d62095058af8cd7b1e697b7b21e4905
admin <p.vilaluz@gmail.com>
2019-09-05 17:52:25 +0100

    ejercicios

--- src/main/java/org/springframework/samples/petclinic/Ejercicios.java
+++ src/main/java/org/springframework/samples/petclinic/Ejercicios.java
@@ -1,42 +1,144 @@
 package org.springframework.samples.petclinic;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.owner.Owner;
+import org.springframework.samples.petclinic.owner.Pet;
+import org.springframework.samples.petclinic.owner.PetType;
 
 public class Ejercicios {
 
-    public static void ejercicio1(Connection connection, Statement statement) throws SQLException {
-        // 1. btener todos los clientes de la clinica y mostrar sus datos por pantalla
-    }
+	public static void ejercicio1(Connection connection, Statement statement) throws SQLException {
+		// 1. btener todos los clientes de la clinica y mostrar sus datos por pantalla
+		statement = connection.createStatement();
+		String sql = "SELECT * FROM owners;";
+		ResultSet rs = statement.executeQuery(sql);
 
-    public static void ejercicio2(Connection connection, Statement statement) throws SQLException {
-        // 2. Insertarnos a nosotros como nuevo propietario de una mascota
+		while (rs.next()) {
+			int id = rs.getInt("id");
+			String firstName = rs.getString("first_name");
+			String lastName = rs.getString("last_name");
+			String address = rs.getString("address");
+			String city = rs.getString("city");
+			String telephone = rs.getString("telephone");
 
-    }
+			System.out.println("Id: " + id);
+			System.out.println(", Nombre: " + firstName);
+			System.out.println(", Apellidos: " + lastName);
+			System.out.println(", Adress: " + address);
+			System.out.println("city: " + city);
+			System.out.println("telephone: " + telephone);
 
-    public static void ejercicio3(Connection connection, Statement statement) throws SQLException {
-        // 3. Modificar nuestra ciudad por Sevilla
-        int numeroDeFilasModificadas = -1;
-        System.out.println("Se han modificado " + numeroDeFilasModificadas + " rows.");
+		}
+		rs.close();
+	}
 
-    }
+	public static void ejercicio2(Connection connection, Statement statement) throws SQLException {
+		// 2. Insertarnos a nosotros como nuevo propietario de una mascota
 
-    public static void ejercicio4(Connection connection) throws SQLException {
-        // 4. Crear una variable de tipo String y buscar todos los Owner que coincidan en nombre o apellido.
+		statement = connection.createStatement();
+		String sql = "INSERT INTO owners(id, first_name, last_name, address, city, telephone) VALUES (null, 'Patricia', 'Vila Luz', 'Lisboa', 'Lisboa', '12345')";
+		int a = statement.executeUpdate(sql);
+	}
 
-    }
+	public static void ejercicio3(Connection connection, Statement statement) throws SQLException {
+		// 3. Modificar nuestra ciudad por Sevilla
+		statement = connection.createStatement();
+		String sql = "UPDATE owners SET city = 'Sevilla' WHERE id=11";
+		statement.executeUpdate(sql);
 
-    public static void ejercicio5(Connection connection) throws SQLException {
-        // 5. Crear a partir de las variables nombre, apellido, direccion, ciudad y teléfono (todas de tipo
-        // String), un nuevo owner
-        int numeroDeFilasModificadas = -1;
-        System.out.println("Se han modificado " + numeroDeFilasModificadas + " rows.");
+	}
 
-    }
+	public static void ejercicio4(Connection connection) throws SQLException {
+		// 4. Crear una variable de tipo String y buscar todos los Owner que coincidan
+		// en nombre o apellido.
+		PreparedStatement preparedStatement = null;
+		String name = "Patricia";
+		String sql = "SELECT * FROM owners WHERE first_name=? OR last_name=?";
+		preparedStatement = connection.prepareStatement(sql);
+		preparedStatement.setString(1, name);
+		preparedStatement.setString(2, name);
+		ResultSet rs = preparedStatement.executeQuery();
 
-    public static void reto(Connection connection, Statement statement) throws SQLException {
+		while (rs.next()) {
+			int id = rs.getInt("id");
+			String firstName = rs.getString("first_name");
+			String lastName = rs.getString("last_name");
 
-    }
+			System.out.println("Id: " + id);
+			System.out.println(", Nombre: " + firstName);
+			System.out.println(", Apellidos: " + lastName);
+		}
+		rs.close();
+	}
 
+	public static void ejercicio5(Connection connection) throws SQLException {
+		// 5. Crear a partir de las variables nombre, apellido, direccion, ciudad y
+		// teléfono (todas de tipo
+		// String), un nuevo owner
+
+		String nombre = "aaa";
+		String apellido = "bbb";
+		String direccion = "ccc";
+		String ciudade = "ddd";
+		String telefono = "eee";
+		String sql = "INSERT INTO owners VALUES (null, ?, ?, ?, ?, ?)";
+		PreparedStatement preparedStatement = null;
+		preparedStatement = connection.prepareStatement(sql);
+		preparedStatement.setString(1, nombre);
+		preparedStatement.setString(2, apellido);
+		preparedStatement.setString(3, direccion);
+		preparedStatement.setString(4, ciudade);
+		preparedStatement.setString(5, telefono);
+		preparedStatement.executeUpdate();
+
+	}
+
+	public static void reto(Connection connection, Statement statement) throws SQLException {
+		Owner patricia = new Owner();
+		Pet elephant = new Pet();
+		Date data = new Date();
+		
+		patricia.setId(null);
+		patricia.setLastName("manel");
+		patricia.setFirstName("manel");
+		patricia.setAddress("debaixo da ponte");
+		patricia.setCity("china");
+		patricia.setTelephone("12345");
+		
+		String sql = "INSERT INTO owners VALUES (null, ?, ?, ?, ?, ?)";
+		PreparedStatement preparedStatement = null;
+		preparedStatement = connection.prepareStatement(sql);
+		preparedStatement.setString(1, patricia.getFirstName());
+		preparedStatement.setString(2, patricia.getFirstName());
+		preparedStatement.setString(3, patricia.getAddress());
+		preparedStatement.setString(4, patricia.getCity());
+		preparedStatement.setString(4, patricia.getTelephone());
+		preparedStatement.executeUpdate();
+		
+		elephant.setId(null);
+		elephant.setName("ze cao");
+		elephant.setBirthDate(data);
+
+		String sql = "INSERT INTO pets VALUES (null, ?, ?, ?, ?)";
+		PreparedStatement preparedStatement = null;
+		preparedStatement = connection.prepareStatement(sql);
+		preparedStatement.setString(1, elephant.getName());
+		preparedStatement.setString(2, elephant.getBirthDate());
+		
+	
+		String sql = "DELETE FROM owners WHERE id=?";
+	}
 }
--- src/main/java/org/springframework/samples/petclinic/JDBCApplication.java
+++ src/main/java/org/springframework/samples/petclinic/JDBCApplication.java
@@ -2,6 +2,8 @@ package org.springframework.samples.petclinic;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -9,57 +11,79 @@ import org.springframework.boot.SpringApplication;
 
 public class JDBCApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PetClinicApplication.class, args);
-        System.out.println("-------- Test de conexión con MySQL ------------");
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicApplication.class, args);
+		System.out.println("-------- Test de conexión con MySQL ------------");
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("No encuentro el driver en el Classpath");
-            e.printStackTrace();
-            return;
-        }
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("No encuentro el driver en el Classpath");
+			e.printStackTrace();
+			return;
+		}
 
-        System.out.println("Driver instalado y funcionando");
-        Connection connection = null;
-        Statement statement = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/petclinic", "root", "root");
-            if (connection != null) {
-                System.out.println("Conexión establecida");
-            }
-            // TODO: hacer aqui los ejercicios del taller en la usando la Class Ejercicios
-            System.out.println("\n\n===== EJERCICIO 1 =====");
-            Ejercicios.ejercicio1(connection, statement);
-            System.out.println("\n\n===== EJERCICIO 2 =====");
-            Ejercicios.ejercicio2(connection, statement);
-            System.out.println("\n\n===== EJERCICIO 3 =====");
-            Ejercicios.ejercicio3(connection, statement);
-            System.out.println("\n\n===== EJERCICIO 4 =====");
-            Ejercicios.ejercicio4(connection);
-            System.out.println("\n\n===== EJERCICIO 5 =====");
-            Ejercicios.ejercicio5(connection);
-            System.out.println(" \n\n===== RETO =====");
-            // Ejercicios.reto();
-        } catch (SQLException e) {
-            System.out.println("Connection Failed! Check output console");
-            e.printStackTrace();
-            return;
-        } finally {
-            try {
-                if (statement != null)
-                    connection.close();
-            } catch (SQLException se) {
+		System.out.println("Driver instalado y funcionando");
+		Connection connection = null;
+		Statement statement = null;
+		PreparedStatement preparedStatement = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/petclinic", "root", "");
+			if (connection != null) {
+				System.out.println("Conexión establecida");
 
-            }
-            try {
-                if (connection != null)
-                    connection.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
-    }
+				statement = connection.createStatement();
+				String sql = "SELECT * FROM vets";
+
+				preparedStatement = connection.prepareStatement(sql);
+//				preparedStatement.setInt(1, 2);
+
+				// ResultSet rs = statement.executeQuery(sql)
+				ResultSet rs = preparedStatement.executeQuery();
+
+				while (rs.next()) {
+					int id = rs.getInt("id");
+					String firstName = rs.getString("first_name");
+					String lastName = rs.getString("last_name");
+
+					System.out.println("Id: " + id);
+					System.out.println(", Nombre: " + firstName);
+					System.out.println(", Apellidos: " + lastName);
+				}
+				rs.close();
+			}
+
+			// TODO: hacer aqui los ejercicios del taller en la usando la Class Ejercicios
+			System.out.println("\n\n===== EJERCICIO 1 =====");
+			Ejercicios.ejercicio1(connection, statement);
+//			System.out.println("\n\n===== EJERCICIO 2 =====");
+//			Ejercicios.ejercicio2(connection, statement);
+			System.out.println("\n\n===== EJERCICIO 3 =====");
+			Ejercicios.ejercicio3(connection, statement);
+			System.out.println("\n\n===== EJERCICIO 4 =====");
+			Ejercicios.ejercicio4(connection);
+			System.out.println("\n\n===== EJERCICIO 5 =====");
+			Ejercicios.ejercicio5(connection);
+			System.out.println(" \n\n===== RETO =====");
+			// Ejercicios.reto();
+		} catch (SQLException e) {
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+		} finally {
+			try {
+				if (statement != null)
+					connection.close();
+			} catch (SQLException se) {
+
+			}
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+	}
 
 }
